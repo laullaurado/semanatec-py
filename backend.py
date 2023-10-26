22e369ffe2e86a8a12f5305c74fe952a69722e8b
laullaurado <a01754196@itesm.mx>
2023-10-26 13:22:45 -0600

    Ruta form y cambio de archivo

--- backend.py
+++ backend.py
@@ -2,14 +2,16 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
 
-dt = joblib.load("./static/dt.joblib")
+dt = joblib.load("./static/dt2.joblib")
 
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route("/hola", methods=["GET"])
 def inicio():
     return "Hola mundo"
+
 
 @app.route("/predict_json", methods=["POST"])
 def predict_json():
@@ -18,9 +20,22 @@ def predict_json():
         float(data["pH"]),
         float(data["sulphates"]),
         float(data["alcohol"])
-        ]]
+    ]]
     y_pred = dt.predict(X)
     return jsonify({"result": y_pred[0]})
+
+
+@app.route("/predict_form", methods=["POST"])
+def predict_form():
+    data = request.form
+    X = [[
+        float(data["pH"]),
+        float(data["sulphates"]),
+        float(data["alcohol"])
+    ]]
+    y_pred = dt.predict(X)
+    return jsonify({"result": y_pred[0]})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=False, port=8081)
